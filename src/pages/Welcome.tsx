9239940dbad1ad9a9c8f16981793da3fbaf44b2f
Ëüclïdës Drý-C <57298973+euclidesdry@users.noreply.github.com>
2021-06-01 16:15:15 +0100

    Adding source page assets

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -1,11 +1,31 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, Image, TouchableOpacity, StyleSheet } from 'react-native';
+
+import wateringImg from '../assets/watering.png';
 
 export function Welcome() {
     return (
-        <view>
-            <Text>Ola!</Text>
-            <Text>NLW #5</Text>
-        </view>
+        <View style={styles.container}>
+            <Text>Cuide facilmente as suas Plantas</Text>
+
+            <Image source={wateringImg} />
+
+            <Text>
+                Não esqueça mais de regar suas plantas.
+                Nós cuidamos de lembrar você sempre que precisar.
+            </Text>
+
+            <TouchableOpacity>
+                >
+            </TouchableOpacity>
+        </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'space-between'
+    }
+})
