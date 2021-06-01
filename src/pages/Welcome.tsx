0438b928800f3266353c41801db83fdc4e21cbe5
Ëüclïdës Drý-C <57298973+euclidesdry@users.noreply.github.com>
2021-06-01 16:28:56 +0100

    Adding colors styles file

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -1,24 +1,25 @@
 import React from 'react';
-import {View, Text, Image, TouchableOpacity, StyleSheet } from 'react-native';
+import { SafeAreaView, View, Text, Image, TouchableOpacity, StyleSheet } from 'react-native';
 
 import wateringImg from '../assets/watering.png';
+import colors from '../styles/colors';
 
 export function Welcome() {
     return (
-        <View style={styles.container}>
-            <Text>Cuide facilmente as suas Plantas</Text>
+        <SafeAreaView style={styles.container}>
+            <Text style={styles.title}>Cuide facilmente as suas Plantas.</Text>
 
             <Image source={wateringImg} />
 
-            <Text>
+            <Text style={styles.subtitle}>
                 Não esqueça mais de regar suas plantas.
                 Nós cuidamos de lembrar você sempre que precisar.
             </Text>
 
-            <TouchableOpacity>
+            <TouchableOpacity style={styles.button}>
                 >
             </TouchableOpacity>
-        </View>
+        </SafeAreaView>
     )
 }
 
@@ -27,5 +28,27 @@ const styles = StyleSheet.create({
         flex: 1,
         alignItems: 'center',
         justifyContent: 'space-between'
+    },
+    title: {
+        fontSize: 32,
+        fontWeight: 'bold',
+        textAlign: 'center',
+        color: colors.heading,
+        marginTop: 38
+    },
+    subtitle: {
+        textAlign: 'center',
+        fontSize: 18,
+        paddingHoriontal: 20,
+        color: colors.heading
+    },
+    button: {
+        backgroudColor: colors.green,
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 16,
+        marginBottom: 10,
+        height: 56,
+        width: 56
     }
 })
