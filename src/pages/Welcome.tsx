303a0ba6d01e59b42ea07a310ea170041e6c6470
Ëüclïdës Drý-C <57298973+euclidesdry@users.noreply.github.com>
2021-06-01 16:02:27 +0100

    Adding assets and custom ts d, creating Welcome file page

---
+++ src/pages/Welcome.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+export function Welcome() {
+    return (
+        <view>
+            <Text>Ola!</Text>
+            <Text>NLW #5</Text>
+        </view>
+    )
+}
