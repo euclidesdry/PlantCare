c1b3977e32e12caa7111d9e57a246b9855648b23
Ëüclïdës Drý-C <57298973+euclidesdry@users.noreply.github.com>
2021-06-06 20:01:41 +0100

    Adding File to config JSON Server

---
+++ src/components/PlantCardPrimary.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+import {
+  StyleSheet,
+  View,
+  Text
+} from "react-native";
+import { RectButton, RectButtonProps } from "react-native-gesture-handler";
+
+// Styles
+import colors from "../styles/colors";
+import fonts from "../styles/fonts";
+
+interface PlantProps extends RectButtonProps {
+  data: {
+    name: string;
+    photo: string;
+  }
+}
+
+export const PlantCardPrimay = ({ data, ...rest }: PlantProps) => {
+  return (
+    <RectButton
+      style={styles.container}
+      {...rest}
+    >
+      <Text style={styles.text}>
+        {data.name}
+      </Text>
+    </RectButton>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    maxWidth: '45%',
+    backgroudColor: colors.shape,
+    borderRadious: 20,
+    paddingVertical: 10,
+    alignItems: 'center',
+    margin: 10
+  },
+  text: {
+    color: colors.green_dark,
+    fontFamily: fonts.heading,
+    marginVertical: 16
+  }
+})
